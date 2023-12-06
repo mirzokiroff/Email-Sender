9c167450c1095171333d27b93263b2022e8d3be5
Mirsidiq Mirzokirov <132640387+mirzokiroff@users.noreply.github.com>
2023-12-06 14:11:58 +0500

    email host password is removed settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,8 @@ CACHES = {
 }
 
 
-EMAIL_HOST_USER = 'oybekn033@gmail.com'
-EMAIL_HOST_PASSWORD = 'lxqqztmlarzwexjm'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
